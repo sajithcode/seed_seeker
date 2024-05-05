038c2c17040d6ccb2e9af4da61ebc953ff1c3e11
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2024-05-06 02:01:19 +0530

    login and register responsive problems fix

--- seed_web/src/app/layout.js
+++ seed_web/src/app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <HeaderOne />
         <HeaderTwo />
-        {/* <HeaderThree /> */}
+        <HeaderThree />
         {children}
         <Footer />
         </body>
