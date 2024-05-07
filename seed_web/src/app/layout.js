e76aaa04288d4dce2e9f1dedaf35380526b1770d
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2024-05-07 14:42:54 +0530

    favicon image update

--- seed_web/src/app/layout.js
+++ seed_web/src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <link rel="icon" href="/favicon.png" sizes="any" />
         <HeaderOne />
         <HeaderTwo />
         <HeaderThree />
