c635793785efbc256212e39b8aaed8e252951bc2
tim-contact <timcontactmail@protonmail.com>
2024-06-12 15:46:23 +0530

    modified dashboard with front-end design

--- seed_web/src/app/dashboard/page.js
+++ seed_web/src/app/dashboard/page.js
@@ -2,9 +2,11 @@ import SeedForm from '@/components/seller_dashboard/seed_form';
 
 const SellerDashboard = () => {
     return (
-        <div>
-            <h1>Seller Dashboard</h1>
-            <SeedForm />
+        <div className='text-center py-5 bg-green-700'>
+            <h1 className='text-3xl block text-white'>Seller Dashboard</h1>
+            <div>
+                <SeedForm/>
+            </div>
         </div>
     );
 };
